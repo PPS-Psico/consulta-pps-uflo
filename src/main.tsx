3916b88e3c0fce05274688cdbcbe8adc449bb99e
megablas <blas.r7@gmail.com>
2025-12-01 00:42:37 -0300

    feat: actualizar componente y limpiar historial

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css' // importa Tailwind y tus estilos globales
 
-// Monta la aplicación en el div #root definido en index.html
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 )
