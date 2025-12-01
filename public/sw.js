28897dd6f96b99606ed871cb6baa197d4339a425
megablas <blas.r7@gmail.com>
2025-12-01 13:14:58 -0300

    feat: actualizar componente y limpiar historial

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 
 // sw.js para relative base
 
-const CACHE_NAME = 'mi-panel-academico-cache-v17';
+const CACHE_NAME = 'mi-panel-academico-cache-v18';
 const FILES_TO_CACHE = [
   './index.html',
   './manifest.json',
