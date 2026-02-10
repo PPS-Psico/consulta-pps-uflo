4231791e1502cba0e6ef3865c950be28770da361
megablas <blas.r7@gmail.com>
2026-02-10 09:43:54 -0300

    feat(sw): auto-update sw via skipWaiting

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -141,6 +141,15 @@ messaging.onBackgroundMessage((payload) => {
   return self.registration.showNotification(title, options);
 });
 
+// Force SW update immediately
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(clients.claim());
+});
+
 // Handle notification click
 self.addEventListener("notificationclick", (event) => {
   console.log("[SW] Notification click received", event);
